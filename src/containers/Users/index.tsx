e1f832d13122fbc39095059a701117efeed541e0
sivani@keyvalue.systems <sivani@keyvalue.systems>
2022-09-25 07:43:35 +0530

    style fixes

--- src/containers/Users/index.tsx
+++ src/containers/Users/index.tsx
@@ -1,75 +1,49 @@
-import React, { useEffect } from "react";
-import Button from "@mui/material/Button";
+import React from "react";
 import { useRecoilState } from "recoil";
 import { tableListAtom, userListAtom } from "../../states/userStates";
 import DeleteIcon from "@mui/icons-material/Delete";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
 import { useQuery } from "@apollo/client";
-import { GET_USERS, GET_USER_GROUPS } from "./services/queries";
-// import TableListing from "../../Components/Table/tableListing";
-import {
-  GridActionsCellItem,
-  GridColumns,
-  GridRowModes,
-  GridRowModesModel,
-  GridRowsProp,
-} from "@mui/x-data-grid";
-import "./index.css";
 import EditIcon from "@mui/icons-material/Edit";
-import TableListing from "../../components/Table/tableListing";
+import { GridActionsCellItem, GridColumns } from "@mui/x-data-grid";
 
+import { GET_USERS, GET_USER_GROUPS } from "./services/queries";
+import "./styles.css";
+import TableListing from "../../components/Table";
 
-// const groupvalues = [
-//   ['aaa', 'bbb'],
-//   ['ccc', 'ddd', 'eee'],
-//   ['fff', 'ggg', 'hhh', 'iii'],
-//   ['ccc', 'ddd', 'eee'],
-//   ['aaa', 'bbb'],
-//   ['fff', 'ggg', 'hhh', 'iii'],
-//   ['aaa', 'bbb'],
-//   ['ccc', 'ddd', 'eee'],
-//   ['ccc', 'ddd', 'eee'],
-//   ['fff', 'ggg', 'hhh', 'iii', 'kkk']
-// ];
-
-// let i: number;
-const arr: any[] = [];
-// const initialRows: GridRowsProp = [
-//   {
-//     id: "",
-//     firstName: "",
-//     actions: ["Edit", "Delete", "Add to Group"]
-//   },
-// ];
 const Users: React.FC = () => {
-  const [rowModesModel] = React.useState<GridRowModesModel>({});
-
   const [userList, setUserList] = useRecoilState(userListAtom);
   // const [tableList, setTableList] = useRecoilState(tableListAtom);
+
+  const { data } = useQuery(GET_USERS, {
+    onCompleted: (data) => {
+      setUserList(data?.getUsers);
+    },
+  });
 
   const columns: GridColumns = [
     {
       field: "firstName",
       headerName: "User",
-      width: 230,
+      width: 300,
       headerClassName: "user-list-header",
+      headerAlign: "left",
     },
     {
       field: "groups",
       headerName: "Member Of",
       headerClassName: "user-list-header",
-      width: 635,
+      flex: 1,
       renderCell: (params) => <ShowGroupList {...params} />,
+      headerAlign: "center",
     },
     {
       field: "actions",
       type: "actions",
       headerName: "Actions",
-      width: 100,
+      flex: 0.5,
       cellClassName: "actions",
+      headerAlign: "center",
       getActions: ({ id }) => {
-        // const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
         return [
           <GridActionsCellItem
             icon={<EditIcon />}
@@ -87,56 +61,16 @@ const Users: React.FC = () => {
     },
   ];
 
-  // eslint - disable - next - line @typescript-eslint / no - unused - vars
-  const {
-    data: data1,
-    loading,
-    refetch,
-  } = useQuery(GET_USERS, {
-    onCompleted: (data1) => {
-      setUserList(data1?.getUsers);
-    },
-  });
-
-  // const { data:data2 } = useQuery(GET_USER_GROUPS);
-  // console.log("data2",data2)
-
-  console.log("data1", data1?.getUsers);
-
-  console.log("hi", userList);
-
-  useEffect(() => {
-    userList.map((user: any, i) => (arr[i] = user.firstName));
-  }, [userList]);
-
-  for (let j = 0; j < arr.length; j++) {
-    console.log("array=", arr[j]);
-  }
-
-  let newUserList = userList.slice();
-  console.log("newUserList", newUserList);
   return (
-    <div>
-      Users
-      {userList.map((user: any, i) => (
-        <div key={i}>{user.firstName}</div>
-      ))}
-      <Button variant="contained">Contained</Button>
-      <Tooltip title="Delete">
-        <IconButton>
-          <DeleteIcon />
-        </IconButton>
-      </Tooltip>
-      <div>
-        <TableListing
-          rows={userList}
-          columns={columns}
-          text="All Users"
-          buttonlabel="Add User"
-          searchlabel="Search User"
-        />
-      </div>
-    </div>
+    <>
+      <TableListing
+        rows={userList}
+        columns={columns}
+        text="All Users"
+        buttonlabel="Add User"
+        searchlabel="Search User"
+      />
+    </>
   );
 };
 
@@ -149,17 +83,20 @@ const ShowGroupList = (props: any) => {
       id: row.id,
     },
     onCompleted: (data) => {
-      // const g: any = [
-      //   { id: "e8be89be-5495-44d8-a112-dd4bce29d6ac", name: "Admin" },
-      //   { id: "e8be89be-5495-44d8-a112-dd4bce29d6ab", name: "Group2" },
-      //   { id: "e8be89be-5495-44d8-a112-dd4bce29d6ak", name: "Group3" },
-      // ];
-      // setGroupList(g);
       setGroupList(data?.getUserGroups);
     },
   });
 
-  return <>{groupList?.map((group: any) => <span className="groups"> {group?.name} </span>)}</>;
+  return (
+    <>
+      {groupList?.map((group: any) => (
+        <span className="groups" key={group.id}>
+          {" "}
+          {group?.name}{" "}
+        </span>
+      ))}
+    </>
+  );
 };
 
 export default Users;
--- src/containers/Users/services/queries.ts
+++ src/containers/Users/services/queries.ts
@@ -1,29 +1,20 @@
-import {gql} from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const GET_USERS = gql`query
-getUsers  {
+export const GET_USERS = gql`
+  query getUsers {
     getUsers {
-        id,
-        email,
-        firstName,
+      id
+      email
+      firstName
     }
-}`
+  }
+`;
 
-// export const GET_USER_GROUPS=gql`query getUsers{    
-//     User{
-//       id,
-//       email,
-//       firstName,
-//       UserGroupResponse{
-//         id,
-//         name,
-//       }
-//     }
-//   }`
-
-export const GET_USER_GROUPS=gql`query($id:ID!){    
-    getUserGroups(id: $id){
+export const GET_USER_GROUPS = gql`
+  query ($id: ID!) {
+    getUserGroups(id: $id) {
       id
       name
     }
-  }`;
+  }
+`;
