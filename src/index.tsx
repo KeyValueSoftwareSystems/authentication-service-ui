33c850c6a033b1cf4502228e4ef2aa7b657df943
shivani <svani5277@gmail.com>
2022-09-27 21:55:35 +0530

    removed conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import {ApolloProvider} from "@apollo/client";
+
 import "./index.css";
 import App from "./App";
 import client from "./services/apolloClient";
-import { RecoilRoot } from "recoil";
-import {ApolloProvider} from "@apollo/client";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 
 root.render(
   <React.StrictMode>
