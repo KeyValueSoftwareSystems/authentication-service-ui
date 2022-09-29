06b1f7da60b4d72edc3e1294ae7cb3e94b09e727
reenu-kv <reenu@keyvalue.systems>
2022-09-29 10:43:08 +0530

    resolved conflicts in index.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
-
 import './App.css';
 import RoutesLayout from './routes/routesLayout';
 import theme from './themes/themes';
