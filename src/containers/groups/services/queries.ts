353bd5479b3cfc014577eefe4b0b5a9e5c307e02
shivani <svani5277@gmail.com>
2022-10-07 13:21:38 +0530

    added group list

---
+++ src/containers/groups/services/queries.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const GET_GROUPS = gql`
+  query getGroups {
+    getGroups {
+      id
+      name
+    }
+  }
+`;
+
+export const GET_GROUP_ROLES = gql`
+  query ($id: ID!) {
+    getGroupRoles(id: $id) {
+      id
+      name
+    }
+  }
+`;
