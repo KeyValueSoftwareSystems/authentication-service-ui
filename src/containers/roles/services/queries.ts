d14e774e28a7c5ab33d59b5a8b26047f87787b2d
Balagopal <balagopal@keyvalue.systems>
2022-10-11 11:15:08 +0530

    fix folder structure

--- src/containers/roles/services/queries.ts
+++ src/containers/roles/services/queries.ts
@@ -16,3 +16,21 @@ export const GET_GROUP_ROLES = gql`
     }
   }
 `;
+
+export const GET_ROLE = gql`
+  query getRole($id: ID!) {
+    getRole(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export const GET_ROLE_PERMISSIONS = gql`
+  query getRolePermissions($id: ID!) {
+    getRolePermissions(id: $id) {
+      id
+      name
+    }
+  }
+`;
--- src/containers/users/services/queries.ts
+++ src/containers/users/services/queries.ts
@@ -1,5 +1,19 @@
 import { gql } from "@apollo/client";
 
+
+export const GET_USER = gql`
+  query getUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      email
+      phone
+      firstName
+      middleName
+      lastName
+    }
+  }
+`;
+
 export const GET_USERS = gql`
   query getUsers {
     getUsers {
