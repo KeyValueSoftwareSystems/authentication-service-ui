fab8dc85cc3fb8c783e48855c6c5a43fc5d44e3c
reenu-kv <reenu@keyvalue.systems>
2022-10-17 14:16:48 +0530

    groupdetails removed from index

--- src/containers/groups/index.tsx
+++ src/containers/groups/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import CreateOrEditGroup from "./components/create-edit/CreateEditGroup";
-import GroupDetails from "./GroupDetails";
 
 const Groups: React.FC = () => {
   return (
