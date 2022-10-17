9c91afbc646f6aeb6f54beccb059877b67906f13
shivani <svani5277@gmail.com>
2022-10-17 15:18:26 +0530

    resolving merge conflicts-added styles.css

--- src/states/groupStates.ts
+++ src/states/groupStates.ts
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 
+export const groupListAtom = atom({
+  key: "GroupList",
+  default: [],
+});
+
 export const groupDetailsAtom = atom({
   key: "GroupDetails",
   default: {
