bceae5763ec650d3fbcbc316eca699fe1b57552a
reenu-kv <reenu@keyvalue.systems>
2022-10-28 13:22:06 +0530

    prettier

--- src/components/filter-chips/FilterChips.tsx
+++ src/components/filter-chips/FilterChips.tsx
@@ -7,15 +7,12 @@ import { GET_PERMISSIONS } from "../../containers/permissions/services/queries";
 import { Permission } from "../../types/user";
 
 interface FilterChipsProps {
-
   selectedPermissions: Permission[];
-  handleClick: (permission:Permission) => void;
+  handleClick: (permission: Permission) => void;
 }
 
-const FilterChips: React.FC<FilterChipsProps> = (
-props:FilterChipsProps
-) => {
-  const {handleClick,selectedPermissions} = props;
+const FilterChips: React.FC<FilterChipsProps> = (props: FilterChipsProps) => {
+  const { handleClick, selectedPermissions } = props;
   const [allPermissions, setAllPermissions] = useState<Permission[]>([]);
   useQuery(GET_PERMISSIONS, {
     onCompleted: (data) => {
@@ -27,16 +24,20 @@ props:FilterChipsProps
     <div className="chips-stack">
       {allPermissions?.map((permission: Permission) => {
         const selected = selectedPermissions
-          .map((selected:Permission) => selected.id)
+          .map((selected: Permission) => selected.id)
           .includes(permission.id);
         return (
           <Chip
-            sx={selected?{
-              fontSize: "medium",
-              borderColor:'#01579B'
-            }:{
-              fontSize: "medium",
-            }}
+            sx={
+              selected
+                ? {
+                    fontSize: "medium",
+                    borderColor: "#01579B",
+                  }
+                : {
+                    fontSize: "medium",
+                  }
+            }
             key={permission.id}
             label={permission.name}
             onClick={() => handleClick(permission)}
