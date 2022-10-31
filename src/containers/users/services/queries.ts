5036e50ab2697867002b87af98bc12492374ba19
Balagopal <balagopal@keyvalue.systems>
2022-10-31 17:46:41 +0530

    refactored code and added minor changes to ui

--- src/containers/users/services/queries.ts
+++ src/containers/users/services/queries.ts
@@ -9,6 +9,14 @@ export const GET_USER = gql`
       firstName
       middleName
       lastName
+      groups {
+        id
+        name
+      }
+      permissions {
+        id
+        name
+      }
     }
   }
 `;
@@ -21,7 +29,7 @@ export const GET_USERS = gql`
       firstName
       middleName
       lastName
-      groups{
+      groups {
         id
         name
       }
