6110853301b797a7ae2625871503e2a694d77890
shivani <svani5277@gmail.com>
2022-12-02 17:47:16 +0530

    sidebar-reusable component

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,5 +1,5 @@
 import { ApolloError, useMutation, useQuery } from "@apollo/client";
-import { Outlet, Navigate, useNavigate, NavLink } from "react-router-dom";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { Avatar, Divider } from "@mui/material";
 import { useRecoilState } from "recoil";
@@ -15,9 +15,7 @@ import { stringAvatar } from "../../utils/table";
 import Toast from "../../components/toast";
 import { apiRequestAtom, toastMessageAtom } from "../../states/apiRequestState";
 import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
-import { ReactComponent as UsersIcon } from "../../assets/users.svg";
-import { ReactComponent as GroupsIcon } from "../../assets/groups.svg";
-import { ReactComponent as RolesIcon } from "../../assets/roles.svg";
+import SideBar from "../../components/side-bar";
 
 const HomePage = () => {
   const setApiSuccess = useSetRecoilState(apiRequestAtom);
@@ -58,15 +56,6 @@ const HomePage = () => {
   return (
     <>
       <div className="wrapperContainer">
-        {/* <div className="navBar">
-          <div className="navLogo">
-            <img alt="logo" src={LOGO_URL} style={{ width: "170px" }} />
-          </div>
-          <div className="menu-icon">
-            <MenuIcon />
-          </div>
-        </div> */}
-
         <div className="body">
           <div className="sideBar">
             <div className="navBar">
@@ -78,48 +67,7 @@ const HomePage = () => {
               </div>
             </div>
             <div className="nav-user-details">
-              <nav>
-                <div className="sideBarContainer">
-                  <NavLink
-                    to="/home/users"
-                    className={({ isActive }) =>
-                      isActive ? "active-text-link" : "text-link"
-                    }
-                  >
-                    <UsersIcon className="sidebar-icon" />
-                    Users
-                  </NavLink>
-                  <NavLink
-                    to="/home/groups"
-                    className={({ isActive }) =>
-                      isActive ? "active-text-link" : "text-link"
-                    }
-                  >
-                    <GroupsIcon className="sidebar-icon" />
-                    Groups
-                  </NavLink>
-
-                  <NavLink
-                    to="/home/roles"
-                    className={({ isActive }) =>
-                      isActive ? "active-text-link" : "text-link"
-                    }
-                  >
-                    <RolesIcon className="sidebar-icon" />
-                    Roles
-                  </NavLink>
-
-                  {/* <NavLink
-                    to="/home/permissions"
-                    className={({ isActive }) =>
-                      isActive ? "active-text-link" : "text-link"
-                    }
-                  >
-                    <LockOutlinedIcon className="icon" />
-                    Permissions
-                  </NavLink> */}
-                </div>
-              </nav>
+              <SideBar />
               <div>
                 <Divider />
                 <div className="userdetails">
