dd5586212f806fef0fdad7c8fbc3823ba280f7b4
Nikhin Raj <85353935+nikhin-keyvalue@users.noreply.github.com>
2023-03-03 16:26:07 +0530

    Develop (#141) (#142)
    * Develop (#141)
    
    Logout integration (Logout integration #32)
    
    made common permissiontabs component,refactoring
    
    Few style changes, added permissions column, removed default column menu of DataGrid
    
    Filtered group/role permissions (Filtered group/role permissions  #39)
    
    handled permissions for edit group
    
    addmembers tab added, code refactoring
    
    Delete Confirmation Popup (Delete Confirmation Popup #49)
    
    confirmation-popup-added
    
    Update current user's permissions on change (Update current user's permissions on change #52)
    
    update current user's permissions on change
    
    to make all avatars of the same user of uniform color
    
    update current user, navigate to login on errors
    
    hiding actions,update custom group's permissions
    
    added tooltip message for copying and refreshing invite links
    
    implemented new checklist for role selection in groups (New checklist for role selection in groups #72)
    
    Implemented pagination changes and fixed API s invoked multiple times
    
    Added message for set password,removed menu icon (Added set password message #101)
    
    Restructured container folders,queries and mutations (Restructured container folders,queries and mutations #107)
    
    Fixed containers folder structure and moved queries and mutations into services
    
    Implemented changes to make user details page responsive
    
    Added maximum character limit for input fields
    
    Fixed ripple effect and inputfield bug (Bug fixes  #127)
    
    delete icon hide,go button, bugs (qa bug fixes #130)
    
    Filter fixes - component made generic (Filter fixes - component made generic #134)
    
    * Adding: prettierignore, exclude- node modules
    
    * MInor bug fix with permissions
    
    * MInor bug fix: edit api fail
    
    ---------
    
    Co-authored-by: Nikhin <nikhinrajkk@gmail.com>
    Co-authored-by: Joelbabu17 <joel.b@keyvalue.systems>

---
+++ src/components/table-toolbar/types.ts
@@ -0,0 +1,20 @@
+import { DocumentNode } from 'graphql';
+import { SetterOrUpdater } from 'recoil';
+
+export interface TableToolBarProps {
+  field: string;
+  buttonLabel: string;
+  searchLabel: string;
+  searchQuery: DocumentNode;
+  currentFilters?: string[][];
+  handleClickFilter?: (event: any, setAnchorEl: (value: React.SetStateAction<null>) => void) => void;
+  handleCancel?: (setAnchorEl: (value: React.SetStateAction<null>) => void) => void;
+  filters?: string[][];
+  checkedFilters?: string[][];
+  setCheckedFilters?: SetterOrUpdater<string[]>[];
+  viewFiltersVerified?: boolean[];
+  setItemList: any;
+  filterName?: string[];
+  isAddVerified?: boolean;
+  onAdd?: () => void;
+}
